25c822cf49e55e6c8c2b34c21293a18422a9b59e
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-27 12:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function toCase(text) {
   // write your code here
+	let str='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	let lower=''
+	let upper=''
+	for(const a of text){
+		if(str.includes(a)){
+			lower+=(a.toLowerCase())
+			upper+=(a.toUpperCase())
+		}
+		else{
+			lower+=(a)
+			upper+=(a)
+		}
+	}
+	return `${lower}-${upper}`
 }
 
 // DO not change the code below
